5f3aa79a0d84b7c3ef9eb0a1d1d886a8dbd05588
jacksongblack <songyuchaoemail@163.com>
2013-11-27 15:42:38 +0800

    对象方式重构代码

--- assets/www/js/ajax.js
+++ assets/www/js/ajax.js
@@ -1,16 +1,54 @@
-//���͵�ַ�ĸ��ַ�����ؽ��Ϊ��ַ���ַ�
+//返回根节点
 function getRootPath() {
-    return "http://192.168.1.109:3000/"
+    return "http://192.168.0.251:3000/"
 };
-//������Ҫ�����HTML�ַ�
+function showLoader() {
+
+    $.mobile.loading('show', {
+        textVisible: true, //ֻ��ʾ�ֵ�����
+        theme: 'b',        //������
+        html: " <a onclick='hideLoader()'  data-icon='delete' data-role='button' class='stopLoader' style='display: none;' >加载中..........点击取消</a>"         //Ҫ��ʾ��html���ݣ���ͼƬ��
+    })
+    $('a.stopLoader').show();
+};
+function hideLoader() {
+    $.mobile.loading('hide');
+};
+//编辑并创建html页面
 function buildHtml(response) {
     var htmlCache = "";
     var newsContent;
     $.each(response, function (n, value) {
-        newsContent = "<li><img src="+ value.logo +" class='ul-li-icon'><h3><a  data-ajax='false' path='api/posts/"+ value.id + "'>" + value.title + "</a></h3>" + "<p>" + value.description + "</p></li>";
+        newsContent = "<li><img src=" + value.logo + " class='ul-li-icon'><h3><a  data-ajax='false' path='api/posts/" + value.id + "'>" + value.title + "</a></h3>" + "<p>" + value.description + "</p></li>";
         newsContent = newsContent + htmlCache;
         htmlCache = newsContent;
     });
     return newsContent;
 };
+function GetTo(url) {
+    this.url = url;
+    _this_= this;
+}
+
+
+GetTo.prototype = {
+    constructor: GetTo,
+    storageName:"flash",
+    send: function () {
+        showLoader();
+        $.getJSON(getRootPath() + this.url, function (response) {
+            sessionStorage.setItem(_this_.storageName, jsonToString(response))
+        });
+        var data = sessionStorage.getItem(_this_.storageName);
+        hideLoader();
+        return stirngToJson(data);
+    },
+    clearSessionStorage: function() {
+        sessionStorage.removeItem(_this_.storageName);
+    }
+}
+
+
+
+
 
--- assets/www/js/tool.js
+++ assets/www/js/tool.js
@@ -1,19 +1,3 @@
-function showLoader() {
-
-    //ï¿½ï¿½Ê¾ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½.for jQuery Mobile 1.2.0
-    $.mobile.loading('show', {
-        textVisible: true, //Ö»ÏÔÊ¾×ÖµÄÄÚÈÝ
-        theme: 'b',        //Ö÷Ìâ·ç¸ñ
-        html: " <a onclick='hideLoader()'  data-icon='delete' data-role='button' class='stopLoader' style='display: none;' >¼ÓÔÚÖÐ....µã»÷È¡Ïû</a>"         //Òªï¿½ï¿½Ê¾ï¿½ï¿½htmlï¿½ï¿½ï¿½Ý£ï¿½ï¿½ï¿½Í¼Æ¬ï¿½ï¿½
-    })
-    $('a.stopLoader').show();
-};
-function hideLoader() {
-    //ï¿½ï¿½ï¿½Ø¼ï¿½ï¿½ï¿½ï¿½ï¿½
-    $.mobile.loading('hide');
-};
-
-
 function jsonToString(jsonObj) {
     return JSON.stringify(jsonObj);
 };
@@ -21,50 +5,45 @@ function stirngToJson(string) {
     return eval('(' + string + ')');
 }
 
-function sessionToJson(session) {
+function flashToJson(session) {
 //    sessionStorage.clear();
     return stirngToJson(session);
 }
 
-function showSimpleLoad(){
-    $.mobile.loading('show',{
+function showSimpleLoad() {
+    $.mobile.loading('show', {
         textVisible: true,
-        text: "¼ÓÔØÖÐ",
+        text: "ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½",
         textonly: false,
         theme: "c"
     })
 }
 function onPullDown() {
-    showSimpleLoad()
     var url = $("#pullAjaxUrl").attr("href") + "1";
-    $.getJSON(getRootPath()+url,function(response){
-        $("[data-role='listview']").empty();
-        $("[data-role='listview']").append(buildHtml(response));
-        $(".news-content").trigger("create")
-        $('.news-list').listview('refresh');
-        $("#pullAjaxUrl").attr("data-page","1")
-        hideLoader();
-        bindLink("a.ui-link");
-    });
+    var getobj = new GetTo(url,"posts");
+    var data = getobj.send();
+    $("[data-role='listview']").empty();
+    $("[data-role='listview']").append(buildHtml(data));
+    $(".news-content").trigger("create")
+    $('.news-list').listview('refresh');
+    $("#pullAjaxUrl").attr("data-page", "1")
+    bindLink("a.ui-link");
 
 };
 function onPullUp() {
-    showSimpleLoad();
- var url = editPath( $("#pullAjaxUrl"));
-    $.getJSON(getRootPath()+url ,function(response){
-        $("[data-role='listview']").append(buildHtml(response));
-        $(".news-content").trigger("create")
-        $('.news-list').listview('refresh');
-        hideLoader();
-        bindLink("a.ui-link");
-    });
-
+    var url = editPath($("#pullAjaxUrl"));
+    var getobj = new GetTo(url);
+    $("[data-role='listview']").append(buildHtml(getobj.send()));
+    $(".news-content").trigger("create")
+    $('.news-list').listview('refresh');
+    bindLink("a.ui-link");
+    getobj.clearSessionStorage()
 };
-function editPath(docObj){
- var pageNumber= docObj.attr("data-page");
-     pageNumber =  new Number(pageNumber) + 1 ;
-     docObj.attr("data-page",pageNumber);
-     return docObj.attr("href") + pageNumber;
+function editPath(docObj) {
+    var pageNumber = docObj.attr("data-page");
+    pageNumber = new Number(pageNumber) + 1;
+    docObj.attr("data-page", pageNumber);
+    return docObj.attr("href") + pageNumber;
 }
 
 

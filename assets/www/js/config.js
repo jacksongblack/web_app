4ecda1ba56fa8f5454719b7fbebf7ce8b3c2ac52
jacksongblack <songyuchaoemail@163.com>
2013-12-09 17:41:49 +0800

    修改企业信息

--- assets/www/js/config.js
+++ assets/www/js/config.js
@@ -9,7 +9,7 @@ SetConfig.prototype.htmlTemplate = {
         return '<li><a data-storageKey="posts" pageTo="news_list.html" path="api/industries/' + obj.id + '/posts">' + obj.name + '</a></li>';
     },
     products_list: function (obj) {
-        return  "<li><a  data-ajax='false' data-storageKey='post' pageTo='product_show.html' path='api/products/" + obj.id + "'><img  class='ul-li-icon' src=''><h3>"
+        return  "<li><a  data-ajax='false' data-storageKey='product' pageTo='product_show.html' path='api/products/" + obj.id + "'><img  class='ul-li-icon' src=''><h3>"
             + obj.name + "</h3>" + "<p>" + obj.description + "</p></a></li>";
     },
     products_theme: function (obj) {
@@ -20,6 +20,10 @@ SetConfig.prototype.htmlTemplate = {
             + obj.title + "</h3>" + "<p>" + obj.description + "</p></a></li>"
     },
     enterprises_theme:function(obj){
-        return '<li><a path="api/industries/' + obj.id + '/posts">' + obj.name + '</a></li>';
+        return '<li><a data-ajax="false" data-storageKey="enterprise" pageTo="enterprises_list.html"  path="api/industries/' + obj.id + '/enterprises">' + obj.name + '</a></li>';
+    },
+    enterprises_list:function(obj){
+        return "<li><a  data-ajax='false'  data-storageKey='company' pageTo='news_show.html' path='api/posts/" + obj.id + "'><img  class='ul-li-icon' src='"+ getRootPath() + obj.logo +"'><h3>"
+            + obj.name + "</h3>" + "<p>邮箱地址:&nbsp" + obj.email+ "</p><p>电话号码:&nbsp"+obj.tel+"</p><p>说明:&nbsp"+obj.description +"</p></a></li>"
     }
 }

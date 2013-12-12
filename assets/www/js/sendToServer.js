f9e7e3d1bf50b9ae3e1d46a5a37d703d3fb7f5cc
jacksongblack <songyuchaoemail@163.com>
2013-12-12 15:54:22 +0800

    修改渲染类的名称

--- assets/www/js/sendToServer.js
+++ assets/www/js/sendToServer.js
@@ -25,7 +25,7 @@ GetTo.prototype = {
             async: false,
 
             error: function () {
-               alert("出错了")
+                _thisGetToObj_.response = "error"
             },
             success: function (data) {
                 _thisGetToObj_.callBack(data)

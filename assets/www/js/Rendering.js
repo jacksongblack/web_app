4df7340c636ef21a04f36ffa3f930d05944ec43f
jacksongblack <songyuchaoemail@163.com>
2013-12-17 17:27:22 +0800

    增加登录界面

--- assets/www/js/Rendering.js
+++ assets/www/js/Rendering.js
@@ -4,6 +4,18 @@ function Rendering() {
 }
 inheritPrototype(Rendering,StorageTool)
 Rendering.prototype.htmlTemplate = {
+    user:function(obj){
+        return "<div class='enterprise'><a href='#'><img id='avatar' style='width:100%; ' src="+ getRootPath() + obj.image_url +" ></a>" +
+            "<div class='content'><h2>公司信息</h2><table>"+
+            "<tr><th>名称:</th><td>" + obj.name + "</td></tr>" +
+            "<tr><th>电话:</th><td>" + obj.tel + "</td></tr>" +
+            "<tr><th>邮箱:</th><td>" + obj.email + "</td></tr>" +
+            "<tr><th>地址:</th><td>" + obj.address + "</td></tr>" +
+            "</table><div class='description' style=''><h2>公司简介</h2><p>"+obj.description+"</p></div>" +
+            "<div class='products_list'><h2>用户操作</h2><a data-mini='true' data-role='button' pageTo='update_user.html' path=/api/industries/"+obj.id+"/products data-storageKey='products' class='producs' >修改产品信息</a></div>" +
+            "</div>" +
+            "</div>"
+    },
     news_theme: function (obj) {
         return  '<li><a path="api/domains/' + obj.id + '/posts" data-storageKey="posts" pageTo="news_list.html">' + obj.name + '</a></li>';
     },
@@ -71,7 +83,6 @@ Rendering.prototype.main ={
     objectToHtml:function(obj){
         var htmlCache = "";
         var newsContent;
-        console.log(obj)
         $.each(obj, function (n, value) {
             newsContent = _thisRendRing_.main.template(value);
             newsContent = newsContent + htmlCache;
--- assets/www/js/login.js
+++ assets/www/js/login.js
@@ -2,33 +2,46 @@ function Login() {
     GetTo.call(this);
     _thisLogin_ = this;
 }
-inheritPrototype(Login, GetTo) ;
+inheritPrototype(Login, GetTo);
 
 Login.prototype.self = {
-    bindButton: function () {
-        var buttonObj = $("#login_button");
-        buttonObj.bind("tap",function () {
-            _thisLogin_.sendType="post"
-            _thisLogin_.data= _thisLogin_.self.getByValue()
-            _thisLogin_.send(getRootPath()+buttonObj.attr("path"));
-            _thisLogin_.self.callback();
+    bindButton: function (docObj) {
+        var buttonObj = docObj;
+        buttonObj.bind("tap", function () {
+            _thisLogin_.sendType = "post"
+            _thisLogin_.data = _thisLogin_.self.getByValue()
+            _thisLogin_.send(getRootPath() + buttonObj.attr("path"));
+            _thisLogin_.self.callback(docObj);
         });
     },
     getByValue: function () {
         var vals = $("#login_form :input").serialize();
         return vals
     },
-    callback: function () {
+    callback: function (docObj) {
         var response = _thisLogin_.getResponse();
-        if (response == "false") {
-            $("#message").popup("open")
-
-        } else {
-            if (response.length != 0) {
-                _thisLogin_.saveResponseTo("user");
-            }
+        if (response.status == "fails") {
+            alert(response.message);
+            return;
         }
-
+        ;
+        if (response.status == "ok") {
+            _thisLogin_.sendType = "get";
+            _thisLogin_.data ="";
+            _thisLogin_.send(getRootPath() + "api/user");
+            console.log(_thisLogin_.getResponse())
+            _thisLogin_.saveResponseTo(docObj.attr("data-storageKey"));
+            _thisLogin_.jumpTo(docObj.attr("pageTo"));
+        }
+    },
+    quit: function () {
+        _thisLogin_.send(getRootPath() + "api/destroy_session");
+        var response = _thisLogin_.getResponse();
+        if (response.status == "ok") {
+            alert(response.message)
+        } else {
+            alert("×¢ÏúÊ§°Ü")
+        }
     }
 
 }

a0b2054563d5444f2f81108c6c4b764209948ec5
jacksongblack <songyuchaoemail@163.com>
2013-12-18 15:25:23 +0800

    上传登录页面

--- assets/www/js/storageTool.js
+++ assets/www/js/storageTool.js
@@ -7,7 +7,7 @@ StorageTool.prototype={
     return JSON.stringify(jsonObj);
 },
 stringToJson:function(string){
-    return eval('(' + string + ')');
+    return eval('(  ' + string + ')');
 },
 sessionClear : function(key){
     localStorage.removeItem(key)

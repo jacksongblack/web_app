bcbd001018eff559166c46eceeb04229f443d655
jacksongblack <songyuchaoemail@163.com>
2013-12-23 17:55:51 +0800

    调试成功的一个单元测试框架

--- assets/www/test_unit/spec/pullIscroll_spec.js
+++ assets/www/test_unit/spec/pullIscroll_spec.js
@@ -1,13 +1,14 @@
+
 describe("拉动刷新测试",function(){
 
     beforeEach(function(){
-        jasmine.getFixtures().fixturesPath = '../../';
+        jasmine.getFixtures().fixturesPath = '../';
+        loadFixtures('news_list.html')
         localStorage.setItem("url","http://0.0.0.0:3000/")
     })
     it("获取页面远程链接地址",function(){
-
-        loadFixtures('news_list.html')
-        expect(pullObj.self.getPath()).toBe("http://0.0.0.0:3000")
+        var pullObj = new PullLoad();
+        expect(pullObj.self.getPath("#pullAjaxUrl")).toBe("api/domains/1?posts_page=2")
     })
 
 })

3fd80c3324ff2c76775f79df246a0f284273ec04
jacksongblack <songyuchaoemail@163.com>
2013-12-23 18:14:58 +0800

    上传新的单元测试

--- assets/www/test_unit/spec/pullIscroll_spec.js
+++ assets/www/test_unit/spec/pullIscroll_spec.js
@@ -5,10 +5,15 @@ describe("拉动刷新测试",function(){
         jasmine.getFixtures().fixturesPath = '../';
         loadFixtures('news_list.html')
         localStorage.setItem("url","http://0.0.0.0:3000/")
+
     })
     it("获取页面远程链接地址",function(){
         var pullObj = new PullLoad();
         expect(pullObj.self.getPath("#pullAjaxUrl")).toBe("api/domains/1?posts_page=2")
+    })
+    it ("测试发送函数",function(){
+        var pullObj = new PullLoad();
+        spyOn(pullObj, 'send');
     })
 
 })
